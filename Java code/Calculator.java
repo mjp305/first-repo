2b4a65a54608507d6fddbdcb2bb8aa10931e98f1
Mike Paul <83442127+mjp305@users.noreply.github.com>
2025-02-23 16:14:14 -0600

    Update Calculator.java

--- Java code/Calculator.java
+++ Java code/Calculator.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+This code is to create a calculator with several functions
  */
 package calculator;
 
